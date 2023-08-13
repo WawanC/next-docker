0e3e8b8cfbaa00aaac74d506dfc671c7ad18fc44
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2023-08-13 21:05:36 +0700

    initial commmit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["www.docker.com"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
